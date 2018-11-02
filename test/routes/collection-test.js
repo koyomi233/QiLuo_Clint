b6b9599528100d5eb43b0781356352272a305fff
Zhiwen Han <317657452@qq.com>
2018-11-02 16:22:29 +0000

    Get collections by name endpoint test

--- test/routes/collection-test.js
+++ test/routes/collection-test.js
@@ -37,5 +37,34 @@ describe('Collection', () => {
                 });
         });
     });
+    describe('GET /collection/names/:name', () => {
+        it('should return several collections which matches the name', function (done) {
+            chai.request(server)
+                .get('/collection/names/illustration')
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(1);
+                    let  result = _.map(res.body, (board) => {
+                        return { category: board.category,
+                            name: board.name }
+                    });
+                    expect(result).to.include( { category: 'Animation', name: 'Japanese illustration'  } );
+                    done();
+                });
+        });
+        it('should return a message for invalid collection name ', function (done) {
+            chai.request(server)
+                .get('/collection/names/Marvel')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message', 'Collection NOT Found!' ) ;
+                    Collection.collection.remove();
+                    Picture.collection.remove();
+                    User.collection.remove();
+                    done();
+                });
+        });
+    });
 
 });
